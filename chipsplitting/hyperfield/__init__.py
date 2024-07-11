f683aaedf7d7bd8904da49b8f7e5099ea1426180
Viet Duc Nguyen <vd.nguyen96@gmail.com>
2024-07-11 20:16:45 +0200

    Reorganize imports

--- chipsplitting/hyperfield/__init__.py
+++ chipsplitting/hyperfield/__init__.py
@@ -1,6 +1,6 @@
-from .hyperfield_vector import HyperfieldVector
 from .contraction_form import HyperfieldContractionForm
 from .hom_linear_system import HyperfieldHomogeneousLinearSystem
+from .hyperfield_vector import HyperfieldVector
 from .linear_form import HyperfieldLinearForm
 from .pascal_form import HyperfieldPascalForm
 from .utils import grid_iter
